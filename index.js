318461785fb66ac8496e17bf1c5caf6cc11971c8
param <param772003@gmail.com>
2024-02-03 20:15:58 +0530

    payload and sendNotification( )

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const path = require("path");
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
 
 const publicVapidKey = `"${process.env.PUBLIC_VAPID_KEY}"`;
@@ -16,4 +18,10 @@ webpush.setVapidDetails("mailto:test@test.com", publicVapidKey, privateVapidKey)
 app.post("/subscribe", (req, res) => {   //Subscribe Route
     const pushSubscription = req.body.subscription;   //Get push subscription object
     res.status(201).json({});   //send 201 status code - resource created
+
+    const payload = JSON.stringify({ title: "Push Test" });   //create payload
+
+    webpush.sendNotification(pushSubscription, payload).catch((err) => console.log(err));   //pass pushSubscription object into sendNotification
 });
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
